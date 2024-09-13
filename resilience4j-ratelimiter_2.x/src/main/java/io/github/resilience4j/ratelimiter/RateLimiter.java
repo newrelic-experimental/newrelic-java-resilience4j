6c7c210280ef53cce38be5d5e1fbefed2795d966
gsidhwani_nr <gsidhwani@newrelic.com>
2024-09-13 15:26:09 +0530

    feat: Added Bulkhead, RateLimiter, Retry, TimeLimiter insturmentation

---
+++ resilience4j-ratelimiter_2.x/src/main/java/io/github/resilience4j/ratelimiter/RateLimiter.java
@@ -0,0 +1,218 @@
+package io.github.resilience4j.ratelimiter;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Future;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import com.newrelic.api.agent.NewRelic;
+import com.newrelic.api.agent.Trace;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+
+import io.github.resilience4j.core.functions.CheckedFunction;
+import io.github.resilience4j.core.functions.CheckedRunnable;
+import io.github.resilience4j.core.functions.CheckedSupplier;
+
+@Weave(type = MatchType.Interface)
+public abstract class RateLimiter {
+
+	public abstract String getName();
+
+	@Trace(dispatcher = true)
+	public <T> T executeCallable(Callable<T> callable) throws Exception {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter", getName(),
+				"executeCallable");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public <T> T executeCheckedSupplier(CheckedSupplier<T> checkedSupplier) throws Throwable {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter", getName(),
+				"executeCheckedSupplier");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public <T> CompletionStage<T> executeCompletionStage(Supplier<CompletionStage<T>> supplier) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter", getName(),
+				"executeCompletionStage");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public void executeRunnable(Runnable runnable) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter", getName(),
+				"executeRunnable");
+		Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public <T> T executeSupplier(Supplier<T> supplier) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter", getName(),
+				"executeSupplier");
+		return Weaver.callOriginal();
+	}
+
+	// Static methods
+	@Trace(dispatcher = true)
+	public static <T> CheckedSupplier<T> decorateCheckedSupplier(RateLimiter rateLimiter, CheckedSupplier<T> supplier) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter",
+				rateLimiter.getName(), "decorateCheckedSupplier");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public static <T> Supplier<CompletionStage<T>> decorateCompletionStage(RateLimiter rateLimiter,
+			Supplier<CompletionStage<T>> supplier) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter",
+				rateLimiter.getName(), "decorateCompletionStage");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public static CheckedRunnable decorateCheckedRunnable(RateLimiter rateLimiter, CheckedRunnable runnable) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter",
+				rateLimiter.getName(), "decorateCheckedRunnable");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public static <T> Callable<T> decorateCallable(RateLimiter rateLimiter, Callable<T> callable) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter",
+				rateLimiter.getName(), "decorateCallable");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public static <T> Supplier<T> decorateSupplier(RateLimiter rateLimiter, Supplier<T> supplier) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter",
+				rateLimiter.getName(), "decorateSupplier");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public static <T> Consumer<T> decorateConsumer(RateLimiter rateLimiter, Consumer<T> consumer) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter",
+				rateLimiter.getName(), "decorateConsumer");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public static Runnable decorateRunnable(RateLimiter rateLimiter, Runnable runnable) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter",
+				rateLimiter.getName(), "decorateRunnable");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public static <T, R> Function<T, R> decorateFunction(RateLimiter rateLimiter, Function<T, R> function) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter",
+				rateLimiter.getName(), "decorateFunction");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public static <T, R> CheckedFunction<T, R> decorateCheckedFunction(RateLimiter rateLimiter,
+			CheckedFunction<T, R> function) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter",
+				rateLimiter.getName(), "decorateCheckedFunction");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public static <T, F extends Future<T>> Supplier<F> decorateFuture(RateLimiter rateLimiter,
+			Supplier<? extends F> supplier) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter",
+				rateLimiter.getName(), "decorateFuture");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public static <T, F extends Future<T>> Supplier<F> decorateFuture(RateLimiter rateLimiter, int permits,
+			Supplier<? extends F> supplier) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter",
+				rateLimiter.getName(), "decorateFuture");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public static <T, R> Function<T, R> decorateFunction(RateLimiter rateLimiter, int permits,
+			Function<T, R> function) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter",
+				rateLimiter.getName(), "decorateFunction");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public static Runnable decorateRunnable(RateLimiter rateLimiter, int permits, Runnable runnable) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter",
+				rateLimiter.getName(), "decorateRunnable");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public static <T> CheckedSupplier<T> decorateCheckedSupplier(RateLimiter rateLimiter, int permits,
+			CheckedSupplier<T> supplier) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter",
+				rateLimiter.getName(), "decorateCheckedSupplier");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public static CheckedRunnable decorateCheckedRunnable(RateLimiter rateLimiter, int permits,
+			CheckedRunnable runnable) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter",
+				rateLimiter.getName(), "decorateCheckedRunnable");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public static <T, R> CheckedFunction<T, R> decorateCheckedFunction(RateLimiter rateLimiter, int permits,
+			CheckedFunction<T, R> function) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter",
+				rateLimiter.getName(), "decorateCheckedFunction");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public static <T, R> CheckedFunction<T, R> decorateCheckedFunction(RateLimiter rateLimiter,
+			Function<T, Integer> permitsCalculator, CheckedFunction<T, R> function) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter",
+				rateLimiter.getName(), "decorateCheckedFunction");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public static <T> Supplier<T> decorateSupplier(RateLimiter rateLimiter, int permits, Supplier<T> supplier) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter",
+				rateLimiter.getName(), "decorateSupplier");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public static <T> Consumer<T> decorateConsumer(RateLimiter rateLimiter, int permits, Consumer<T> consumer) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter",
+				rateLimiter.getName(), "decorateConsumer");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public static <T, R> Function<T, R> decorateFunction(RateLimiter rateLimiter,
+			Function<T, Integer> permitsCalculator, Function<T, R> function) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter",
+				rateLimiter.getName(), "decorateFunction");
+		return Weaver.callOriginal();
+	}
+
+	@Trace(dispatcher = true)
+	public static <T> Callable<T> decorateCallable(RateLimiter rateLimiter, int permits, Callable<T> callable) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom", "Resilience4j", "RateLimiter",
+				rateLimiter.getName(), "decorateCallable");
+		return Weaver.callOriginal();
+	}
+
+}
